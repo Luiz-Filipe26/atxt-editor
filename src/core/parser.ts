6319227531a54829550035487bc349dcd112bd34
Luiz-Filipe-26 <luizferamos26@gmail.com>
2026-03-17 02:34:34 -0300

    refactor(parser): move tokens input to parse(), use AST namespace, consumeTargetLine now returns BlockNode

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -1,39 +1,18 @@
-import { TokenType, type Token } from "./lexer";
+import { TokenType, type Token } from "../types/tokens";
 import { TokenStream } from "./tokenStream";
-import {
-    NodeType,
-    type DocumentNode,
-    type BlockNode,
-    type TextNode,
-    type AnnotationNode,
-    type PropertyNode,
-    type AnnotationDirective,
-    type BlockContentNode,
-    type TargetNode,
-} from "../types/ast";
+import * as AST from "../types/ast";
 import type { CompilerError } from "../types/errors";
 
 export class Parser {
-    private stream: TokenStream;
+    private stream!: TokenStream;
     private compilerErrors: CompilerError[] = [];
 
-    constructor(tokens: Token[]) {
+    parse(tokens: Token[]): { document: AST.DocumentNode; errors: CompilerError[] } {
         this.stream = new TokenStream(tokens);
-    }
+        this.compilerErrors = [];
 
-    private pushError(message: string, token?: Token) {
-        const currentToken = token ?? this.stream.peek();
-        this.compilerErrors.push({
-            type: "PARSER",
-            message,
-            line: currentToken.line,
-            column: currentToken.column,
-        });
-    }
-
-    public parse(): { document: DocumentNode; errors: CompilerError[] } {
-        const root: DocumentNode = {
-            type: NodeType.DOCUMENT,
+        const root: AST.DocumentNode = {
+            type: AST.NodeType.DOCUMENT,
             line: 1,
             column: 1,
             children: [],
@@ -47,7 +26,7 @@ export class Parser {
         return { document: root, errors: this.compilerErrors };
     }
 
-    private parseNextNode(): BlockContentNode | null {
+    private parseNextNode(): AST.BlockContentNode | null {
         const token = this.stream.advance();
         switch (token.type) {
             case TokenType.ANNOTATION_OPEN:
@@ -71,7 +50,7 @@ export class Parser {
         }
     }
 
-    private parseAnnotation(openingToken: Token): AnnotationNode | null {
+    private parseAnnotation(openingToken: Token): AST.AnnotationNode | null {
         const directive = this.consumeDirective();
         const props = this.parseProperties();
         this.consumeAnnotationClosure();
@@ -85,7 +64,7 @@ export class Parser {
         const needsTarget = directive === "NORMAL" && hasNormalProps;
 
         return {
-            type: NodeType.ANNOTATION,
+            type: AST.NodeType.ANNOTATION,
             line: openingToken.line,
             column: openingToken.column,
             directive: directive,
@@ -94,7 +73,48 @@ export class Parser {
         };
     }
 
-    private resolveAnnotationTarget(): TargetNode | null {
+    private parseBlock(blockToken: Token): AST.BlockNode {
+        const children: AST.BlockContentNode[] = [];
+        while (!this.stream.isAtEnd() && this.stream.peek().type !== TokenType.BLOCK_CLOSE) {
+            const node = this.parseNextNode();
+            if (node) children.push(node);
+        }
+
+        if (!this.stream.match(TokenType.BLOCK_CLOSE)) {
+            this.pushError("Unclosed block. Expected '}'.");
+        }
+
+        return {
+            type: AST.NodeType.BLOCK,
+            line: blockToken.line,
+            column: blockToken.column,
+            children: children,
+        };
+    }
+
+    private parseTextLine(startToken: Token): AST.TextNode {
+        let content = startToken.literal;
+
+        if (startToken.type === TokenType.NEWLINE) {
+            return this.buildTextNode(startToken, content);
+        }
+
+        while (!this.stream.isAtEnd()) {
+            const nextToken = this.stream.peek();
+            if (nextToken.type !== TokenType.TEXT && nextToken.type !== TokenType.NEWLINE) {
+                break;
+            }
+            if (nextToken.type === TokenType.NEWLINE) {
+                content += this.stream.advance().literal;
+                break;
+            }
+            content += this.stream.advance().literal;
+        }
+
+        return this.buildTextNode(startToken, content);
+    }
+
+    private resolveAnnotationTarget(): AST.TargetNode | null {
         if (this.stream.isAtEnd()) return null;
 
         if (this.stream.isTargetingBlock()) {
@@ -106,95 +126,71 @@ export class Parser {
         return this.consumeTargetLine();
     }
 
-    private consumeTargetLine(): TextNode | null {
+    private consumeTargetLine(): AST.BlockNode | null {
         if (this.stream.isAtEnd()) return null;
 
         const startToken = this.stream.peek();
-        let aggregatedText = "";
+        const children: AST.BlockContentNode[] = [];
 
         while (!this.stream.isAtEnd()) {
             const token = this.stream.peek();
 
-            if (token.type === TokenType.NEWLINE) {
-                break;
-            }
+            if (token.type === TokenType.NEWLINE) break;
 
-            if (token.type === TokenType.TEXT) {
-                aggregatedText += this.stream.advance().literal;
+            if (token.type === TokenType.ANNOTATION_OPEN) {
+                this.stream.advance();
+                const node = this.parseAnnotation(token);
+                if (node) children.push(node);
+            } else if (token.type === TokenType.TEXT) {
+                children.push(this.buildTextNode(token, this.stream.advance().literal));
             } else {
                 break;
             }
         }
 
+        if (children.length === 0) return null;
+
         return {
-            type: NodeType.TEXT,
+            type: AST.NodeType.BLOCK,
             line: startToken.line,
             column: startToken.column,
-            content: aggregatedText,
+            children,
         };
     }
 
-    private consumeDirective(): AnnotationDirective {
+    private consumeDirective(): AST.AnnotationDirective {
         const token = this.stream.peek();
-        if (
+        const isDirectiveKeyword =
             token.type === TokenType.IDENTIFIER &&
-            ["SET", "DEFINE", "HIDE"].includes(token.literal)
-        )
-            return this.stream.advance().literal as AnnotationDirective;
+            ["SET", "DEFINE", "HIDE"].includes(token.literal);
+
+        if (isDirectiveKeyword) return this.stream.advance().literal as AST.AnnotationDirective;
         return "NORMAL";
     }
 
-    private parseProperties(): PropertyNode[] {
-        const props: PropertyNode[] = [];
+    private parseProperties(): AST.PropertyNode[] {
+        const props: AST.PropertyNode[] = [];
 
-        while (
-            !this.stream.isAtEnd() &&
-            this.stream.peek().type !== TokenType.ANNOTATION_CLOSE
-        ) {
+        while (!this.stream.isAtEnd() && this.stream.peek().type !== TokenType.ANNOTATION_CLOSE) {
             if (this.stream.match(TokenType.SEMICOLON)) continue;
 
             const keyToken = this.stream.peek();
             const rawKey = this.parsePropertyKey();
             if (!rawKey) continue;
 
-            let toggle: PropertyNode["toggle"] = undefined;
-            let key = rawKey;
-
-            if (rawKey.startsWith("+")) {
-                toggle = "plus";
-                key = rawKey.substring(1);
-            } else if (rawKey.startsWith("-")) {
-                toggle = "minus";
-                key = rawKey.substring(1);
-            }
+            const { key, toggle } = this.parseTogglePrefix(rawKey);
 
             if (toggle === "minus") {
-                props.push({
-                    type: NodeType.PROPERTY,
-                    line: keyToken.line,
-                    column: keyToken.column,
-                    key,
-                    value: "",
-                    toggle,
-                });
+                props.push(this.buildPropertyNode(keyToken, key, "", toggle));
                 this.requirePropertySeparator(rawKey);
                 continue;
             }
 
             if (!this.consumeColon(key)) continue;
-
             const value = this.parsePropertyValue(key);
             if (!value) continue;
 
-            props.push({
-                type: NodeType.PROPERTY,
-                line: keyToken.line,
-                column: keyToken.column,
-                key,
-                value,
-                toggle,
-            });
-
+            props.push(this.buildPropertyNode(keyToken, key, value, toggle));
             this.requirePropertySeparator(rawKey);
         }
 
@@ -203,9 +199,7 @@ export class Parser {
 
     private parsePropertyKey(): string | null {
         if (this.stream.peek().type !== TokenType.IDENTIFIER) {
-            this.pushError(
-                `Expected property name, found '${this.stream.peek().literal}'.`,
-            );
+            this.pushError(`Expected property name, found '${this.stream.peek().literal}'.`);
             this.synchronizeToNextProperty();
             return null;
         }
@@ -236,6 +230,12 @@ export class Parser {
         return this.stream.advance().literal;
     }
 
+    private consumeAnnotationClosure(): void {
+        if (!this.stream.match(TokenType.ANNOTATION_CLOSE)) {
+            this.pushError("Annotation was not closed with ']]'.");
+        }
+    }
+
     private requirePropertySeparator(propertyName: string): void {
         const afterValue = this.stream.peek().type;
         if (afterValue === TokenType.SEMICOLON) {
@@ -243,12 +243,6 @@ export class Parser {
         } else if (afterValue !== TokenType.ANNOTATION_CLOSE) {
             this.pushError(`Expected ';' after property value '${propertyName}'.`);
             this.synchronizeToNextProperty();
-        }
-    }
-
-    private consumeAnnotationClosure(): void {
-        if (!this.stream.match(TokenType.ANNOTATION_CLOSE)) {
-            this.pushError("Annotation was not closed with ']]'.");
         }
     }
 
@@ -260,59 +254,47 @@ export class Parser {
         }
     }
 
-    private parseBlock(blockToken: Token): BlockNode {
-        const children: BlockContentNode[] = [];
-        while (
-            !this.stream.isAtEnd() &&
-            this.stream.peek().type !== TokenType.BLOCK_CLOSE
-        ) {
-            const node = this.parseNextNode();
-            if (node) children.push(node);
-        }
-
-        if (!this.stream.match(TokenType.BLOCK_CLOSE)) {
-            this.pushError("Unclosed block. Expected '}'.");
-        }
-
-        return {
-            type: NodeType.BLOCK,
-            line: blockToken.line,
-            column: blockToken.column,
-            children: children,
-        };
+    private parseTogglePrefix(rawKey: string): {
+        key: string;
+        toggle: AST.PropertyNode["toggle"];
+    } {
+        const toggle: AST.PropertyNode["toggle"] =
+            rawKey[0] === "+" ? "plus" : rawKey[0] === "-" ? "minus" : undefined;
+        return { key: toggle !== undefined ? rawKey.substring(1) : rawKey, toggle };
     }
 
-    private parseTextLine(startToken: Token): TextNode {
-        let content = startToken.literal;
-
-        if (startToken.type === TokenType.NEWLINE) {
-            return this.buildTextNode(startToken, content);
-        }
-
-        while (!this.stream.isAtEnd()) {
-            const nextToken = this.stream.peek();
-            if (
-                nextToken.type !== TokenType.TEXT &&
-                nextToken.type !== TokenType.NEWLINE
-            ) {
-                break;
-            }
-            if (nextToken.type === TokenType.NEWLINE) {
-                content += this.stream.advance().literal;
-                break;
-            }
-            content += this.stream.advance().literal;
-        }
-
-        return this.buildTextNode(startToken, content);
-    }
-
-    private buildTextNode(startToken: Token, content: string): TextNode {
+    private buildTextNode(startToken: Token, content: string): AST.TextNode {
         return {
-            type: NodeType.TEXT,
+            type: AST.NodeType.TEXT,
             line: startToken.line,
             column: startToken.column,
-            content: content,
+            content,
         };
+    }
+
+    private buildPropertyNode(
+        keyToken: Token,
+        key: string,
+        value: string,
+        toggle: AST.PropertyNode["toggle"],
+    ): AST.PropertyNode {
+        return {
+            type: AST.NodeType.PROPERTY,
+            line: keyToken.line,
+            column: keyToken.column,
+            key,
+            value,
+            toggle,
+        };
+    }
+
+    private pushError(message: string, token?: Token) {
+        const currentToken = token ?? this.stream.peek();
+        this.compilerErrors.push({
+            type: "PARSER",
+            message,
+            line: currentToken.line,
+            column: currentToken.column,
+        });
     }
 }
